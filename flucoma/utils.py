5857a564104b1179cce5ba35ce520d6b220af02b
James Bradbury <jamesbradbury93@gmail.com>
2020-06-09 16:18:43 +0100

    handle the return of command line processes graciously

--- flucoma/utils.py
+++ flucoma/utils.py
@@ -23,3 +23,7 @@ def fftformat(fftsettings: List[int]) -> int:
     if  fftsize == -1:
         fftsize = fftsettings[0]
     return math.floor(2 ** math.ceil(math.log(fftsize)/math.log(2)))
+def handle_ret(retval: int):
+    """Handle return value and raise exceptions if necessary"""
+    if retval != 0:
+        raise ShellError(retval)
