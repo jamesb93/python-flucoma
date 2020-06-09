7be8068f4f3edc2bc117d8a4be3112282fa62924
James Bradbury <jamesbradbury93@gmail.com>
2020-06-09 16:17:20 +0100

    add the shell error exception

--- flucoma/exceptions.py
+++ flucoma/exceptions.py
@@ -1,3 +1,7 @@
 class BinError(Exception):
     def __init__(self, msg:str):
         super().__init__(msg)
+
+class ShellError(Exception):
+    def __init__(self, msg:str):
+        super().__init__(msg)
