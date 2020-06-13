70c1baa52479025903a260f9f4ba60260c5cd455
James Bradbury <jamesbradbury93@gmail.com>
2020-06-13 17:39:42 +0100

    change to typing module and inherit from NamedTuple

--- flucoma/returns.py
+++ flucoma/returns.py
@@ -1,6 +1,19 @@
-import collections
+from typing import NamedTuple
 
-nmf_return = collections.namedtuple('nmf', 'resynth bases activations')
-hpss_return = collections.namedtuple('hpss', 'harmonic percussive residual')
-sines_return = collections.namedtuple('sines', 'sines residual')
-transients_return = collections.namedtuple('transients', 'transients residual')
+class NMFReturn(NamedTuple):
+    resynth:str = ''
+    bases:str = ''
+    activations:str = ''
+
+class HPSSReturn(NamedTuple):
+    harmonic:str = ''
+    percussive:str = ''
+    residual:str = ''
+
+class SinesReturn(NamedTuple):
+    sines:str = ''
+    residual:str = ''
+
+class TransientsReturn(NamedTuple):
+    transients:str = ''
+    residual:str = ''
