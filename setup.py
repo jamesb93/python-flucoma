166f2817977782c91c4a911b4ad6c309b8ee464e
James Bradbury <jamesbradbury93@gmail.com>
2020-06-14 00:58:31 +0100

    use a long description

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 import setuptools
+from pathlib import Path
+
+readme = Path("readme.md")
+
+with open(readme, encoding="utf-8") as f:
+    long_description = f.read()
 
 setuptools.setup(
     name="python-flucoma",
-    version="1.0",
+    version="1.0.0",
     author="James Bradbury",
     author_email="jamesbradbury93@gmail.com",
     description="Loose bindings to the FluCoMa command line",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
     packages=["flucoma"],
     install_requires=[
         "SoundFile",
