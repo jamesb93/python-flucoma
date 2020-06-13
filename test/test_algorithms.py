b103d3d907fce357eadd6d66fce0e175755de044
James Bradbury <jamesbradbury93@gmail.com>
2020-06-13 22:53:16 +0100

    tests

---
+++ test/test_algorithms.py
@@ -0,0 +1,44 @@
+from flucoma import fluid
+from flucoma.utils import get_buffer
+from scipy.io import wavfile
+import numpy as np
+import os
+
+test_file = "test_file.wav"
+
+# slicers
+def test_transientslice():
+    output = fluid.transientslice(test_file)
+    result = get_buffer(output)
+    os.remove(output)
+    tolerance = 25
+    assert len(result) == 1
+    assert result[0] - 512 <= tolerance
+    assert result[0] != -1.0
+
+def test_noveltyslice():
+    output = fluid.noveltyslice(test_file, fftsettings=[128, 64, -1], threshold=0.05)
+    result = get_buffer(output)
+    os.remove(output)
+    tolerance = 128 #one hop
+    assert len(result) == 1
+    assert result[0] - 512 <= tolerance
+    assert result[0] != -1.0
+
+
+def test_ampslice():
+    output = fluid.ampslice(test_file, onthreshold=-24)
+    result = get_buffer(output)
+    os.remove(output)
+    tolerance = 10
+    assert len(result) == 1
+    assert result[0] - 818 <= tolerance
+    assert result[0] != -1.0
+
+def test_ampgate():
+    output = fluid.ampgate(test_file)
+    result = get_buffer(output)
+    os.remove(output)
+    assert len(result) == 2
+    assert result[0][0] == 511
+    assert result[1][0] == 1023
---
+++ test/test_utils.py
@@ -0,0 +1,63 @@
+import pytest
+import numpy as np
+import os
+from random import random
+from scipy.io import wavfile
+
+
+from flucoma.utils import (
+    get_buffer,
+    odd_snap,
+    fftformat,
+    make_temp,
+)
+
+def test_get_buffer():
+    # make a test wave file with some values
+    rands = []
+    test_file = "random_buffer.wav"
+    for i in range(100):
+        rands.append(random())
+    rands_np = np.array(rands)
+    wavfile.write(test_file, 44100, np.array(rands))
+    assert os.path.exists("random_buffer.wav")
+    buflist = get_buffer(test_file)
+    bufnp = get_buffer(test_file, "numpy")
+
+    assert type(buflist) == type(list())
+    assert type(bufnp) == type(np.zeros((1, 1)))
+    assert buflist == rands
+
+
+def test_odd_snap():
+    assert odd_snap(0) == 1
+    assert odd_snap(-24) == -23
+    assert odd_snap(-23) == -23
+    assert odd_snap(2) == 3
+    assert odd_snap(512) == 513
+    assert odd_snap(1025) == 1025
+
+
+def test_fftformat():
+    caseone = [1024, 512, -1]
+    casetwo = [100, 512, -1]
+    casethree = [100, 512, -1]
+    casefour = [100, 512, 100]
+    casefive = [500, -1, -1]
+    assert fftformat(caseone) == 1024
+    assert fftformat(casetwo) == 128
+    assert fftformat(casethree) == 128
+    assert fftformat(casefour) == 128
+    assert fftformat(casefive) == 512
+
+def test_make_temp():
+    temp_file = make_temp()
+    another_file = make_temp()
+    assert type(temp_file) == type(str())
+    assert type(another_file) == type(str())
+    assert another_file != temp_file
+
+
+
+
+
