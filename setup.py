fb585b4b687a79251b1ff389796bb6d2d29f4aae
James Bradbury <jamesbradbury93@gmail.com>
2020-06-14 00:30:27 +0100

    bump to version 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="python-flucoma",
-    version="0.1",
+    version="1.0",
     author="James Bradbury",
     author_email="jamesbradbury93@gmail.com",
     description="Loose bindings to the FluCoMa command line",
