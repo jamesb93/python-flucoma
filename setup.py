bcf4064e9c388a2ac283e6eacfa1f46182b9b3e9
James Bradbury <jamesbradbury93@gmail.com>
2020-07-03 18:42:45 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ with open(readme, encoding="utf-8") as f:
 
 setuptools.setup(
     name="python-flucoma",
-    version="1.0.6",
+    version="1.0.8",
     author="James Bradbury",
     url="https://github.com/jamesb93/python-flucoma",
     author_email="jamesbradbury93@gmail.com",
     description="Loose bindings to the FluCoMa command line",
     license="BSD",
-    long_description = long_description,
-    long_description_content_type = "text/markdown",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=["flucoma"],
     install_requires=[
         "SoundFile",
