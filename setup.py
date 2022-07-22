4d8a71cf310abee543ec6e6d171215c464fb4796
James Bradbury <github@jamesbradbury.net>
2022-07-22 11:58:30 +0100

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(readme, encoding="utf-8") as f:
 
 setuptools.setup(
     name="python-flucoma",
-    version="1.3.0",
+    version="1.3.1",
     author="James Bradbury",
     url="https://github.com/jamesb93/python-flucoma",
     author_email="reacoma@jamesbradbury.net",
